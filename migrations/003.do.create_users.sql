fdd3a600aa70b7b143df9f8759c12a7acbd797a1
Daniel Ors <do7811@gmail.com>
2019-04-23 14:46:11 -0400

    added template for chron job!

---
+++ migrations/003.do.create_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users  (
+  id SERIAL PRIMARY KEY,
+  user_name TEXT NOT NULL UNIQUE,
+  full_name TEXT NOT NULL,
+  password TEXT NOT NULL,
+  date_created TIMESTAMP NOT NULL DEFAULT now()
+);
