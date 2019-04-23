cf6b92230d0449e383552c4878babf3e2c5a06fd
Daniel Ors <do7811@gmail.com>
2019-04-23 11:26:31 -0400

    updated database seeding and migrate

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://pfm@localhost/pet-food',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://danielors@localhost/congress',
+  JWT_SECRET: process.env.JWT_SECRET || 'i-like-lemons',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 };
