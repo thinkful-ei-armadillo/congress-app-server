ebd290adc77d376a58df6a2d939f779254731fc0
Irem Sen <secilreel@yahoo.com>
2019-05-03 11:02:48 -0400

    corrected the bug with members the get all member endpoint need to be leftJoin instead of innerJoin

--- src/utils/extract.js
+++ src/utils/extract.js
@@ -33,8 +33,7 @@ const getMembersObj = obj => {
     senate_class,
     state_rank,
     missed_votes_pct,
-    votes_with_party_pct,
-    user_id
+    votes_with_party_pct
   } = obj;
   return {
     id,
@@ -68,8 +67,7 @@ const getMembersObj = obj => {
     senate_class,
     state_rank,
     missed_votes_pct,
-    votes_with_party_pct,
-    user_id
+    votes_with_party_pct
   };
 };
 
