346bfdaabc185f97c83ec031395138e3d8c9a602
Daniel Ors <do7811@gmail.com>
2019-05-07 23:19:50 -0400

    testing committees endpoint

---
+++ test/committees-endpoint.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+const knex = require('knex');
+const supertest = require('supertest');
+const { expect } = require('chai');
+
+describe('Committees Router Endpoints', () => {
+  let db;
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL_DOS
+    });
+    app.set('db', db);
+  });
+
+});
